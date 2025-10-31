eaf2ab463bc96a329a0015dc6ce24e62a0186ab7
Suresh Bhitrakoti Thapa <gracebarsat@gmail.com>
2025-10-31 18:45:58 -0500

    Added filter by date, category, payment type

--- app.py
+++ app.py
@@ -3,34 +3,52 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 expenses = []
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', expenses=expenses)
+    filtered = expenses
+
+    # Filters applied if method GET + query params exist
+    if request.method == 'GET':
+        date = request.args.get('date')
+        category = request.args.get('category')
+        payment_type = request.args.get('payment_type')
+
+        # Apply filters
+        if date:
+            filtered = [e for e in filtered if e['date'] == date]
+        if category and category != "All":
+            filtered = [e for e in filtered if e['category'] == category]
+        if payment_type and payment_type != "All":
+            filtered = [e for e in filtered if e['payment'] == payment_type]
+
+    return render_template('index.html', expenses=filtered)
 
 @app.route('/add', methods=['POST'])
 def add_expense():
     name = request.form.get('name')
     amount = request.form.get('amount')
-    item_type = request.form.get('item_type')
-    payment_type = request.form.get('payment_type')
-    
-    if name and amount and item_type and payment_type:
+    category = request.form.get('category')
+    payment = request.form.get('payment')
+    date = request.form.get('date')
+
+    if name and amount and category and payment and date:
         expenses.append({
             'sn': len(expenses) + 1,
             'name': name,
             'amount': amount,
-            'item_type': item_type,
-            'payment_type': payment_type
+            'category': category,
+            'payment': payment,
+            'date': date
         })
-    
+
     return redirect('/')
 
 @app.route('/test')
 def test():
     return "<h1>Hello Flask!</h1>"
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
